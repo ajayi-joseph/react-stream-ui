7d5fcef1fa90fc0fd4fd6dc86a6cf2064143b7b2
Joseph Ajayi <adekoyeajayi@gmail.com>
2026-04-30 15:43:42 +0100

    Refactor index.ts to export AssistantMessage type and remove Message type

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,9 @@ export type { UseStructuredOutputResult } from "./hooks/useStructuredOutput.js";
 export { parsePartialJSON } from "./parsers/partial-json.js";
 export type { PartialJSONResult } from "./parsers/partial-json.js";
 export type {
+  AssistantMessage,
   ContentBlock,
   FinishReason,
-  Message,
   StreamChunk,
   StreamSource,
   TextBlock,
